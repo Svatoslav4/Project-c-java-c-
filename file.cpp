ee39ffc32d61f6c416618770b2f2bba54fb5f0d5
Svatoslav Kyshiei <145762491+Svatoslav4@users.noreply.github.com>
2023-10-20 11:59:37 +0300

    file.cpp

---
+++ file.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int price;
+    std::string productName;
+
+    std::cout << "Введіть назву продукту: ";
+    std::cin >> productName;
+    std::cout << "Введіть ціну продукту: ";
+    std::cin >> price;
+
+    std::cout << "Ви внесли продукт: " << productName << " за ціною " << price << " гривень." << std::endl;
+
+    return 0;
+}
